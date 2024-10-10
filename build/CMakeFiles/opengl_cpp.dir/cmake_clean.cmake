6e859d2324be0474960dc19ff5b4f01d825a99f6
henriFox.W <62045411+Henri-lab@users.noreply.github.com>
2024-10-10 15:27:00 +0800

    init

---
+++ build/CMakeFiles/opengl_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/Users/henrifox/opengl_code/opengl_cpp"
+  "/Users/henrifox/opengl_code/opengl_cpp.pdb"
+  "CMakeFiles/opengl_cpp.dir/src/_.cpp.o"
+  "CMakeFiles/opengl_cpp.dir/src/_.cpp.o.d"
+  "CMakeFiles/opengl_cpp.dir/src/main.cpp.o"
+  "CMakeFiles/opengl_cpp.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
